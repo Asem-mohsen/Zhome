394ad0de0d8058b71e8a8e3de5623134a34a1430
Assem Mohsen . <AsMohsen>
2024-05-09 09:58:25 +0300

    New

---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Http\Controllers\Admin\AdminController;
+use App\Http\Controllers\Admin\RolesController;
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('Admin.index');
+});
+
+
+Route::controller(UserController::class)->prefix('Users')->name('Users.')->group(function(){
+        Route::get('/', 'index')->name('index');
+        Route::get('/{$id}/profile', 'profile')->name('profile');
+});
+
+Route::prefix('Admins')->name('Admins.')->group(function(){
+    Route::controller(AdminController::class)->group(function(){
+        Route::get('/', 'index')->name('index');
+        Route::get('/{$id}/profile', 'profile')->name('profile');
+        Route::get('/create', 'create')->name('create');
+    });
+    Route::controller(RolesController::class)->group(function(){
+        Route::get('/Roles', 'index')->name('roles');
+        Route::get('/Roles/create', 'create')->name('roles.create');
+    });
+});
